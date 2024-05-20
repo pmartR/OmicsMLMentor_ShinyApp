7e76bf9928c4edc351ef5d3b37a948aa5abdac3c
Evan Glasscock <evan.glasscock@pnnl.gov>
2024-05-20 11:32:01 -0700

    fix: fix download bundle and downloading

--- Frontend_render_UI/Subpages/Download/Download_buttons.R
+++ Frontend_render_UI/Subpages/Download/Download_buttons.R
@@ -17,18 +17,49 @@ observeEvent(input$makezipfile, {
   temp_dir <- tempdir()
   setwd(temp_dir)
   
+  browser()
+  
   fname <- file.path(temp_dir, 
                      paste0("SLOPE_", method, 
                             "_", gsub("( |:|-)", "_", Sys.time())))
 
-  plots <- reactiveValuesToList(plot_table_current)$table
-  tables <- reactiveValuesToList(table_table_current)$table
+  plots_chk <- reactiveValuesToList(plot_table_current)$table
+  tables_chk <- reactiveValuesToList(table_table_current)$table
   
   plots_keep <- getShinyInput(input, "checkplot")
   tables_keep <- getShinyInput(input, "checktable")
+  plots_keep <- unlist(sapply(1:length(plots_keep), \(x) if (plots_keep[x] == 1) x else NULL))
+  tables_keep <- unlist(sapply(1:length(tables_keep), \(x) if (tables_keep[x] == 1) x else NULL))
   
-  plots_export <- plots[plots_keep]
-  tables_export <- tables[tables_keep]
+  plot_idx <- NULL
+  
+  for (pg in c("Upload", "QC", "MSU", "PP", "RM")) {
+    plot_table <- tibble(
+      rows = plot_table_current$names[which(startsWith(names(plot_table_current$table), paste0(pg, "__")))],
+      list_el = plot_table_current$table[which(startsWith(names(plot_table_current$table), paste0(pg, "__")))] 
+    ) %>% filter(!map_lgl(list_el, is.null))
+    
+    table_with_checks <- plot_table[which(map_lgl(plot_table$rows, \(x) !is.null(x))),]
+    
+    if (any(names(plots_chk) %in% names(table_with_checks$list_el)))
+      plot_idx <- c(plot_idx, which(names(plots_chk) %in% names(table_with_checks$list_el)))
+  }
+  
+  tbl_idx <- NULL
+  for (pg in c("Upload", "QC", "MSU", "PP", "RM")) {
+    table_table <- tibble(
+      rows = table_table_current$names[which(startsWith(names(table_table_current$table), paste0(pg, "__")))],
+      list_el = table_table_current$table[which(startsWith(names(table_table_current$table), paste0(pg, "__")))] 
+    ) %>% filter(!map_lgl(list_el, is.null))
+    
+    table_with_checks <- table_table[which(map_lgl(table_table$rows, \(x) !is.null(x))),]
+    
+    if (any(names(tables_chk) %in% names(table_with_checks$list_el)))
+      tbl_idx <- c(tbl_idx, which(names(tables_chk) %in% names(table_with_checks$list_el)))
+  }
+  
+  plots_export <- plots_chk[plot_idx][plots_keep]
+  tables_export <- tables_chk[tbl_idx][tables_keep]
   
   # Write plots
   if (length(plots_export) > 0) {
@@ -36,17 +67,22 @@ observeEvent(input$makezipfile, {
     withProgress(message = "Writing plot files...", {
       
       file_names_plots <- map2(plots_export, names(plots_export), function(plot, handle){
-        ggsave(file.path(tempdir(), paste0(handle, ".png")), 
-               plot = plot#, 
-               # width = save_options$width, 
-               # height = save_options$height, 
-               # scale = save_options$scale, 
-               # units="px"
-               )
-        
+        if (inherits(plot, "plotly")) {
+          fname <- paste0(handle, ".html")
+          saveWidget(plot, fname)
+        } else {
+          fname <- paste0(handle, ".png")
+          ggsave(fname, 
+                 plot = plot#, 
+                 # width = save_options$width, 
+                 # height = save_options$height, 
+                 # scale = save_options$scale, 
+                 # units="px"
+                 )
+        }
         incProgress(1 / length(plots_export), 
-                    detail = paste0(paste0(handle, ".png"), " done"))
-        file.path(tempdir(), paste0(handle, ".png"))
+                    detail = paste0(fname, " done"))
+        fname
       })
     })
     
@@ -61,12 +97,12 @@ observeEvent(input$makezipfile, {
                                 names(tables_export), function(table, handle){
         
         write.csv(table, 
-                  file = file.path(tempdir(), paste0(handle, ".csv")), 
+                  file = paste0(handle, ".csv"), 
                   row.names = FALSE)
         
         incProgress(1 / length(tables_export), 
                     detail = paste0(paste0(handle, ".csv"), " done"))
-        file.path(tempdir(), paste0(handle, ".csv"))
+        paste0(handle, ".csv")
       })
     })
   } else file_names_tables <- NULL
@@ -75,7 +111,7 @@ observeEvent(input$makezipfile, {
   
   # Write report
   
-  zip(zipfile = fname, 
+  zip(zipfile = paste0(fname, ".zip"), 
       files = c(file_names_tables, file_names_plots), 
       flags = "-r")
   
@@ -85,21 +121,11 @@ observeEvent(input$makezipfile, {
 
 ### Download button ###
 output$download_processed_data <- downloadHandler(
-  filename = paste0("SLOPE_output_", gsub("( |:|-)", "_", Sys.time()), ".zip"),
+  filename = function() {
+    paste0("SLOPE_output_", gsub("( |:|-)", "_", Sys.time()), ".zip")
+  },
   content = function(fname) {
-    
-    # this is necessary to zip up the entire trelliscope directory
-    # specifically, we dont want to have to use the -j flag to get non-directory files..
-    # .. so, we navigate to where everything is (tempdir()) and download with just -r
-    
-    orig_wd <- getwd()
-    on.exit(setwd(orig_wd))
-    setwd(tempdir())
-    
-    zip(zipfile = fname, files = zipped_file$fs, flags = "-r")
-    if (file.exists(paste0(fname, ".zip"))) {
-      file.rename(paste0(fname, ".zip"), fname)
-    }
+    file.copy(zipped_file$fs, fname)
   },
   contentType = "application/zip"
 )
