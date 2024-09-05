182a59eec63e328d84c02f3215a5598f1e3e5a09
Evan Glasscock <evan.glasscock@pnnl.gov>
2024-09-05 13:43:41 -0700

    feat: store PP user inputs

--- Frontend_render_UI/Subpages/PP/PP_progress_render.R
+++ Frontend_render_UI/Subpages/PP/PP_progress_render.R
@@ -7,6 +7,57 @@ output$preprocessing_progress_summary_table <- renderDT({
 })
 
 output$preprocessing_progress_next_steps <- renderUI({
+  subset_names <- c(
+    "No Subsetting" = "all",
+    "Top L order statistics (los)" = "los",
+    "Percentage present (ppp)" = "ppp",
+    "Complete" = "complete",
+    "Rank invariant (rip)" = "rip",
+    "Percentage present and rank invariant (ppp+rip)" = "ppp_rip"
+  )
+  
+  subset_name <- names(subset_names)[
+    which(subset_names == input[[paste0(get_omicsData_type(omicsData$objQC), "_subset_fn")]])
+  ]
+  
+  if (length(subset_name) == 0) {
+    subset_name <- NULL
+    rollup_method <- NULL
+  } else {
+    rollup_method <- switch(
+      input$qc_which_rollup, 
+      rollup = "Centering only",
+      rrollup = "Reference",
+      qrollup = paste0("Quantile (cutoff at ", input$qc_qrollup_thresh, ")"),
+      zrollup = "Z-Score"
+    )
+  }
+  
+  user_inputs$pp <- list(
+    transform = input$transform,
+    molfilt = input[[paste0(get_omicsData_type(omicsData$objQC), "_add_molfilt")]],
+    molfilt_min_num = input[[paste0(get_omicsData_type(omicsData$objQC), "_mol_min_num")]] %>% as.character(),
+    imputefilt = input[[paste0(get_omicsData_type(omicsData$objQC), "_add_imputefilt")]],
+    handle_impute = ifelse(is.null(missingHandleSliderValsFilter()$md_impute), "None", paste(missingHandleSliderValsFilter()$md_impute, collapse = "-")),
+    handle_convert = ifelse(is.null(missingHandleSliderValsFilter()$md_convert), "None", paste(missingHandleSliderValsFilter()$md_convert, collapse = "-")),
+    handle_remove = ifelse(is.null(missingHandleSliderValsFilter()$md_remove), "None", paste(missingHandleSliderValsFilter()$md_remove, collapse = "-")),
+    cvfilt = input[[paste0(get_omicsData_type(omicsData$objQC), "_add_cvfilt")]],
+    cvfilt_threshold = input[[paste0(get_omicsData_type(omicsData$objQC), "_cv_threshold")]],
+    cvfilt_use_groups = input[[paste0(get_omicsData_type(omicsData$objQC), "_cvfilt_use_groups")]],
+    customfilt = input[[paste0(get_omicsData_type(omicsData$objQC), "_add_edata_customfilt")]],
+    customfilt_handling = input[[paste0(get_omicsData_type(omicsData$objQC), "_edata_remove_or_keep")]],
+    customfilt_regex = input[[paste0(get_omicsData_type(omicsData$objQC), "_edata_customfilt_regex")]],
+    norm_method = input[[paste0(get_omicsData_type(omicsData$objQC), "_normalize_option")]],
+    norm_fn = input[[paste0(get_omicsData_type(omicsData$objQC), "_norm_fn")]],
+    norm_subset_fn = subset_name,
+    norm_los = input[[paste0(get_omicsData_type(omicsData$objQC), "_los")]] %>% as.character(),
+    norm_ppp = input[[paste0(get_omicsData_type(omicsData$objQC), "_ppp")]] %>% as.character(),
+    norm_rip = input[[paste0(get_omicsData_type(omicsData$objQC), "_rip")]] %>% as.character(),
+    norm_backtransform = input[[paste0(get_omicsData_type(omicsData$objQC), "_backtransform")]],
+    rollup_method = rollup_method,
+    rollup_combine_fn = input$qc_which_combine_fn
+  )
+  
   tagList(
     tags$b("Run Model"),
     tags$ul(
