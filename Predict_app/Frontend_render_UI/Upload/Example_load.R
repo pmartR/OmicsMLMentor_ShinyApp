09534a7777874923dcef8fcb0278e39a568e519e
Richardson, Rachel E <rachel.richardson@pnnl.gov>
2025-01-13 13:57:27 -0800

    add example data and update model saving

--- Predict_app/Frontend_render_UI/Upload/Example_load.R
+++ Predict_app/Frontend_render_UI/Upload/Example_load.R
@@ -6,7 +6,7 @@
 
 observeEvent(input$load_example, {
   
-  reactive_dataholder$model <- readRDS("./Predict_app/example/data/full_model.RDS")
+  reactive_dataholder$model <- readRDS("./Predict_app/example/data/Full_model_SLOPE_model_2025_01_13_13_34_31.159147.RDS")
   reactive_dataholder$e_data <- read.csv("./Predict_app/example/data/e_data_short.csv")
   reactive_dataholder$e_meta <- read.csv("./Predict_app/example/data/e_meta.csv")
   reactive_dataholder$f_data <- read.csv("./Predict_app/example/data/f_data_short.csv")
